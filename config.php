d72e83b9f2d63d6ab8b3e09d6707ad693a6dfd9d
Eboni1 <joshuamarifrancis@gmail.com>
2026-03-11 13:46:34 +0800

    database nuke fix

--- config.php
+++ config.php
@@ -6,6 +6,8 @@ $database = 'pims';
 
 $conn = new mysqli($host, $username, $password, $database);
 
+mysqli_query($conn, "SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'");
+
 if ($conn->connect_error) {
     // Check if this is an AJAX request
     if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
